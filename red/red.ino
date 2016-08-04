24df6b62ace97b8c407ad65f7d924cc066b8f626
Edgar E. Mamani A <edgar.eucebio.mamani.apaza@gmail.com>
2016-08-03 23:33:56 -0400

    Se modifico el archivo red.ino para mandar por el método POST un JSON

--- red/red.ino
+++ red/red.ino
@@ -1,13 +1,15 @@
 #include <Ethernet.h>
 #include <SPI.h>
 
+#define nombreCosa "iotUMSS"
+
 byte mac[] = {0XAA, 0XBB, 0XCC, 0XDD, 0XEE, 0XFF};
-IPAddress ip(10, 42, 0, 110);
+IPAddress ip(192, 168, 1, 110);
 IPAddress serverDns(8, 8, 8, 8);
-IPAddress gateway(10, 42, 0, 32);
+IPAddress gateway(192, 168, 1, 1);
 EthernetClient miCliente;
 
-int aleatorio;
+int val1 = 0, val2 = 0;
 
 char miServidor[] = "www.dweet.io";
 	
@@ -16,13 +18,41 @@ void setup() {
 	Ethernet.begin(mac, ip, serverDns, gateway);
 	delay(1000);
 
-	randomSeed(millis());
+	randomSeed(A0);
 }
 
 void loop() {
-	aleatorio = random(1, 100);
-	Serial.println(aleatorio);
-
-	//httpPeticion();
+	val1 = random(1, 100);
+	val2 = random(1, 100);
+	httpPeticion(val1, val2);
 	delay(5000);
+}
+
+void httpPeticion(int val1, int val2){
+	miCliente.stop();
+	String json = "{ \"val1\": " + String(val1) + ",\n  \"val2\": " + String(val2) + "\n}";
+
+	if(miCliente.connect(miServidor, 80)){
+		Serial.println("Conectado al servidor...");
+
+		String c = "POST /dweet/for/";
+		c.concat(nombreCosa);
+		c.concat(" HTTP/1.1");		
+		miCliente.println(c);
+		miCliente.println("Host: www.dweet.io");
+		miCliente.println("Content-Type: application/json");
+		miCliente.print("Content-Length: ");
+		miCliente.println(json.length());
+		miCliente.println("Connection: close");
+		miCliente.println();
+		miCliente.println(json);
+
+		Serial.println(json);
+
+		//peticion();
+		
+	}
+	else
+		Serial.println("La conexión con el servidor falló...");
+
 }
